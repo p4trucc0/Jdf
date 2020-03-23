30fccf62b304c3efea2e215b2ac9fa8f4f9de39e
p4trucc0 <patrucco@outlook.it>
2020-03-23 20:26:22 +0100

    Added "mc" command for mathematical operations between columns.

--- javaDataFrame/JdfParser.java
+++ javaDataFrame/JdfParser.java
@@ -102,6 +102,104 @@ public class JdfParser{
 			case "ms":
 				this.out_str = this.out_str.concat("========== MATH OP. WITH SCALAR ==========\n");
 				key_split = this.keyarg.split("=");
+				if (key_split.length < 2) //ok
+				{
+					this.out_str = this.out_str.concat("ERROR: wrong number of arguments.\n");
+					this.err_level = 1;
+					this.err_code = 2; // wrong number of args.
+					return;
+				} 
+				else
+				{
+					col_name = key_split[0];
+					math_expr = key_split[1];
+					math_expr_split = math_expr.split("\\+|/|\\*|\\^|\\-");
+					boolean valid_col_name = !this.jdf.hasColByName(col_name); // Check if column already there.
+					if (!valid_col_name) //ok.
+					{
+						this.out_str = this.out_str.concat("ERROR: Invalid target column name (Already taken!).\n");
+						this.err_level = 1;
+						this.err_code = 6; // invalid column name - already taken.
+						return;
+					}
+					else
+					{
+						if (math_expr_split.length != 2) // ok
+						{
+							this.out_str = this.out_str.concat("ERROR: Wrong number of operands in expression.\n");
+							this.err_level = 1;
+							this.err_code = 5; // wrong number of operands.
+							return;
+						}
+						else
+						{
+							// TODO: Simplify this and recycle code for "mc" command!
+							String src_col_name = math_expr_split[0];
+							String sec_operand = math_expr_split[1];
+							valid_col_name = this.jdf.hasColByNameAndType(src_col_name, "Double");
+							if (!valid_col_name) // ok
+							{
+								this.out_str = this.out_str.concat("ERROR: Invalid column name.\n");
+								this.err_level = 1;
+								this.err_code = 3; // invalid column name
+								return;
+							}
+							else
+							{
+								String opc = ""; // operation character
+								boolean found_opc = false;
+								if (math_expr.contains("+"))
+								{
+									opc = "+";
+									found_opc = true;
+								}
+								if (math_expr.contains("-"))
+								{
+									opc = "-";
+									found_opc = true;
+								}
+								if (math_expr.contains("/"))
+								{
+									opc = "/";
+									found_opc = true;
+								}
+								if (math_expr.contains("*"))
+								{
+									opc = "*";
+									found_opc = true;
+								}
+								if (math_expr.contains("^"))
+								{
+									opc = "^";
+									found_opc = true;
+								}
+								if (!found_opc) // will never get there as it won't be recognized as a regex separator in first place...
+								{
+									this.out_str = this.out_str.concat("ERROR: Unrecognized/unsupported operator.\n");
+									this.err_level = 1;
+									this.err_code = 7; // Unsupported operator.
+									return;
+								}
+								else
+								{
+									// TODO: Check if conversion has been performed successfully.
+									double sop = Double.parseDouble(sec_operand);
+									this.out_str = this.out_str.concat("Defining new column ").concat(col_name).concat(" as ").concat(src_col_name);
+									this.out_str = this.out_str.concat(" ").concat(opc).concat(" ").concat(sec_operand).concat(".\n");
+									this.out_str = this.out_str.concat("DONE.\n");
+									this.jdf.mathOpScalar(col_name, src_col_name, sop, opc);
+									this.err_level = 0;
+									this.err_code = 0;
+									return;
+								}
+							}
+						}
+					}
+				}
+			// MC: mathematical operations between columns.
+			case "mc":
+				this.out_str = this.out_str.concat("========== MATH OP. (2 COLUMNS) ==========\n");
+				key_split = this.keyarg.split("=");
 				if (key_split.length < 2)
 				{
 					this.out_str = this.out_str.concat("ERROR: wrong number of arguments.\n");
@@ -117,7 +215,7 @@ public class JdfParser{
 					boolean valid_col_name = !this.jdf.hasColByName(col_name); // Check if column already there.
 					if (!valid_col_name)
 					{
-						this.out_str = this.out_str.concat("ERROR: Invalid column name (Already taken!).\n");
+						this.out_str = this.out_str.concat("ERROR: Invalid target column name (Already taken!).\n");
 						this.err_level = 1;
 						this.err_code = 6; // invalid column name - already taken.
 						return;
@@ -133,13 +231,12 @@ public class JdfParser{
 						}
 						else
 						{
-							// TODO: Simplify this and recycle code for "mc" command!
-							String src_col_name = math_expr_split[0];
-							String sec_operand = math_expr_split[1];
-							valid_col_name = this.jdf.hasColByNameAndType(src_col_name, "Double");
+							String src_col1_name = math_expr_split[0];
+							String src_col2_name = math_expr_split[1];
+							valid_col_name = ((this.jdf.hasColByNameAndType(src_col1_name, "Double")) && (this.jdf.hasColByNameAndType(src_col2_name, "Double")));
 							if (!valid_col_name)
 							{
-								this.out_str = this.out_str.concat("ERROR: Invalid column name.\n");
+								this.out_str = this.out_str.concat("ERROR: At least one of the specified columns does not exist.\n");
 								this.err_level = 1;
 								this.err_code = 3; // invalid column name
 								return;
@@ -182,12 +279,10 @@ public class JdfParser{
 								}
 								else
 								{
-									// TODO: Check if conversion has been performed successfully.
-									double sop = Double.parseDouble(sec_operand);
-									this.out_str = this.out_str.concat("Defining new column ").concat(col_name).concat(" as ").concat(src_col_name);
-									this.out_str = this.out_str.concat(" ").concat(opc).concat(" ").concat(sec_operand).concat(".\n");
+									this.out_str = this.out_str.concat("Defining new column ").concat(col_name).concat(" as ").concat(src_col1_name);
+									this.out_str = this.out_str.concat(" ").concat(opc).concat(" ").concat(src_col2_name).concat(".\n");
 									this.out_str = this.out_str.concat("DONE.\n");
-									this.jdf.mathOpScalar(col_name, src_col_name, sop, opc);
+									this.jdf.mathOpTwoCol(col_name, src_col1_name, src_col2_name, opc);
 									this.err_level = 0;
 									this.err_code = 0;
 									return;
@@ -222,6 +317,7 @@ public class JdfParser{
 	{
 		this.cmd_dict.addString("f");
 		this.cmd_dict.addString("ms");
+		this.cmd_dict.addString("mc");
 	}
 
 }
